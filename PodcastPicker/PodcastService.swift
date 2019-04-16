707df85e5d3617c6805fb1b7d269ae47e9f1e0f5
Chelsea Troy <chelsea.dommert@gmail.com>
2019-04-15 23:10:00 -0400

    Fetch results from an API call and display them in a list

---
+++ PodcastPicker/PodcastService.swift
@@ -0,0 +1,57 @@
+//
+//  PodcastService.swift
+//  PodcastPicker
+//
+//  Created by Chelsea Troy on 4/15/19.
+//  Copyright © 2019 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+struct PodcastResult: Codable {
+    let resultCount: Int
+    let results: [Podcast]
+}
+
+struct Podcast: Codable {
+    let artistName: String
+    let trackName: String
+    let genres: [String]
+}
+
+class PodcastService {
+    var dataTask: URLSessionDataTask?
+    
+    private let urlString = "https://itunes.apple.com/search?country=US&media=podcast&limit=20&term="
+    
+    func search(for searchTerm: String, completion: @escaping ([Podcast]?, Error?) -> ()) {
+        
+        guard let url = URL(string: urlString + searchTerm) else {
+            print("invalid url: \(urlString + searchTerm)")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
+                DispatchQueue.main.async { completion(nil, error) }
+                return
+            }
+            
+            print("Status code: \(response.statusCode)")
+            print(String(data: data, encoding: .utf8) ?? "unable to print data")
+            
+            do {
+                let decoder = JSONDecoder()
+                let podcastResult = try decoder.decode(PodcastResult.self, from: data)
+                DispatchQueue.main.async { completion(podcastResult.results, nil) }
+            } catch (let error) {
+                
+                DispatchQueue.main.async { completion(nil, error) }
+                
+            }
+        }
+        task.resume()
+    }
+}
+
--- PodcastPicker/ViewController.swift
+++ PodcastPicker/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var podcasts: [Podcast] = []
+    var podcastService: PodcastService!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        let activityIndicator = UIActivityIndicatorView(frame: CGRect(
+            x: self.view.center.x,
+            y: self.view.center.y,
+            width: 20,
+            height: 20
+        ))
+        self.view.addSubview(activityIndicator)
+        
+        self.podcastService = PodcastService()
+        self.podcastService.search(for: "comedy", completion: { podcasts, error in
+            guard let podcasts = podcasts, error == nil else {
+                print(error ?? "unknown error")
+                return
+            }
+            self.podcasts = podcasts
+            self.tableView.reloadData()
+            activityIndicator.isHidden = true
+        })
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.podcasts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let podcast = podcasts[indexPath.row]
+        
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "podcastCell", for: indexPath)
+        cell.textLabel?.text = podcast.trackName
+        cell.detailTextLabel?.text = podcast.artistName
+        
+        return cell
+    }
 }
 
